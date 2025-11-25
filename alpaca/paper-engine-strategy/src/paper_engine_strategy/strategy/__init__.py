aef1eeda25b1fd92841f2be82140077841300a29
JoaoCruz <118855753+JoaoMSSCruz@users.noreply.github.com>
2025-11-25 19:09:18 +0000

    Import SMA __init__

--- alpaca/paper-engine-strategy/src/paper_engine_strategy/strategy/__init__.py
+++ alpaca/paper-engine-strategy/src/paper_engine_strategy/strategy/__init__.py
@@ -1,5 +1,5 @@
 from paper_engine_strategy.strategy.po_hurst_exp import POHurstExpStrategy
-
+from paper_engine_strategy.strategy.sma_strategy import SMAStrategy
 
 __all__ = [
     "POHurstExpStrategy"
